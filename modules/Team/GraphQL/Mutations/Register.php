64fe1a88a7e2098d4eada97390e88d1de6dfffbc
tima <tima.stella88@gmail.com>
2025-11-19 15:42:48 +0200

    update api

---
+++ modules/Team/GraphQL/Mutations/Register.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Team\GraphQL\Mutations;
+
+use GraphQL\Error\Error;
+
+class Register
+{
+    /**
+     * @param null $_
+     * @param array<string, mixed> $args
+     * @throws
+     */
+    public function __invoke(null $_, array $args)
+    {
+        try {
+            dd(212);
+        } catch (Error $error) {
+            throw new Error($error->getMessage());
+        }
+    }
+}
