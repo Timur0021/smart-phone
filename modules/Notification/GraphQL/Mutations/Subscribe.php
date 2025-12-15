94f49da7fdee9a965a5b82e715c1a5e92ed2b907
tima <tima.stella88@gmail.com>
2025-12-15 19:03:17 +0200

    update

---
+++ modules/Notification/GraphQL/Mutations/Subscribe.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Modules\Notification\GraphQL\Mutations;
+
+use GraphQL\Error\Error;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+use Modules\Notification\Models\Subscribe as SubscribeModel;
+
+class Subscribe
+{
+    /**
+     * @param array{}
+     * $args
+     * @throws Error
+     * @throws Throwable
+     */
+    public function __invoke(null $_, array $args): array
+    {
+        DB::beginTransaction();
+        try {
+            $email = $args['email'] ?? null;
+
+            if (empty($email)) {
+                throw new Error('Емейл не введений');
+            }
+
+            SubscribeModel::query()
+                ->firstOrCreate([
+                    'email' => $email
+                ]);
+
+            DB::commit();
+
+            return [
+                'status' => 'Успіх!',
+                'message' => 'Ви успішно підписались'
+            ];
+        } catch (Error $error) {
+            throw new Error($error->getMessage());
+        }
+    }
+}
